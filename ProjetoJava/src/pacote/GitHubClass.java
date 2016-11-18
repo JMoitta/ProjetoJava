f33e7759d4a7d8377e151e33888259a98dfa2479
Joao Moita Manrique <joaomoitamanrique@gmail.com>
2016-11-18 20:01:33 -0200

    a primorando o codigo

--- ProjetoJava/src/pacote/GitHubClass.java
+++ ProjetoJava/src/pacote/GitHubClass.java
@@ -1,7 +1,10 @@
 package pacote;
 
+import java.util.Scanner;
+
 public class GitHubClass {
 	public static void main(String[] args) {
+		Scanner leia = new Scanner(System.in);
 		System.out.println("Eclipse alteracoes");
 	}
 }
