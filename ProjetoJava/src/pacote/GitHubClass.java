f488d06c059f753ba4ffeedee1d76ad9588ff9b6
Joao Moita Manrique <joaomoitamanrique@gmail.com>
2016-11-18 19:57:28 -0200

    Alteracao na mesagem

--- ProjetoJava/src/pacote/GitHubClass.java
+++ ProjetoJava/src/pacote/GitHubClass.java
@@ -2,6 +2,6 @@ package pacote;
 
 public class GitHubClass {
 	public static void main(String[] args) {
-		System.out.println("Testando o GitHUB PELO ECLIPSE");
+		System.out.println("Eclipse alteracoes");
 	}
 }
